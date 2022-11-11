06b85885fda6e8c6c944b58ad31a4da6223b76f8
Michael Goetz <digital.goetz@gmail.com>
2022-11-11 13:33:19 -0500

    updated some docs

--- scripts/request.sh
+++ scripts/request.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export INSPECTOR=$(kubectl get all -n traffic | grep inspector | grep pod | awk '{ print $1 }')
+echo "retrieving obs from $INSPECTOR"
 kubectl exec -it $INSPECTOR -- bash -c ./fetch.sh
